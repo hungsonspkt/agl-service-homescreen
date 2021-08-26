d24581cda395ccd592db6c9b17b17689235f92c2
DESKTOP-5J11CSC\SonLe <hungsonspkt@gmail.com>
2021-08-26 18:30:59 +0700

    update

--- src/hs-appinfo.cpp
+++ src/hs-appinfo.cpp
@@ -19,6 +19,15 @@
 #include "hs-appinfo.h"
 #include "hs-helper.h"
 #include "hs-clientmanager.h"
+
+
+#include <stdio.h>      // standard input / output functions
+#include <stdlib.h>
+#include <string.h>     // string function definitions
+#include <unistd.h>     // UNIX standard function definitions
+#include <fcntl.h>      // File control definitions
+#include <errno.h>      // Error number definitions
+#include <termios.h>    // POSIX terminal control definitions
 #include <pthread.h>
 
 #define RETRY_CNT 10
