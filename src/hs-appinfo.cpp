e560862767da7505cc98ea9d3ceaed2ec38c4bd5
DESKTOP-5J11CSC\SonLe <hungsonspkt@gmail.com>
2021-08-26 18:26:25 +0700

    update

--- src/hs-appinfo.cpp
+++ src/hs-appinfo.cpp
@@ -19,6 +19,7 @@
 #include "hs-appinfo.h"
 #include "hs-helper.h"
 #include "hs-clientmanager.h"
+#include <pthread.h>
 
 #define RETRY_CNT 10
 
@@ -92,6 +93,71 @@ HS_AppInfo::~HS_AppInfo()
         delete afmmain;
 }
 
+pthread_t tid;
+int fdUSB = 0x00;
+
+void printserial()
+{
+    fdUSB = open( "/dev/ttyS0", O_RDWR| O_NOCTTY );
+    struct termios tty;
+    struct termios tty_old;
+    memset (&tty, 0, sizeof tty);
+
+    /* Error Handling */
+    if ( tcgetattr ( fdUSB, &tty ) != 0 ) {
+       printf("error tcgetattr\n");
+       return;
+    }
+
+    /* Save old tty parameters */
+    tty_old = tty;
+
+    /* Set Baud Rate */
+    cfsetospeed (&tty, (speed_t)B115200);
+    cfsetispeed (&tty, (speed_t)B115200);
+
+    /* Setting other Port Stuff */
+    tty.c_cflag     &=  ~PARENB;            // Make 8n1
+    tty.c_cflag     &=  ~CSTOPB;
+    tty.c_cflag     &=  ~CSIZE;
+    tty.c_cflag     |=  CS8;
+
+    tty.c_cflag     &=  ~CRTSCTS;           // no flow control
+    tty.c_cc[VMIN]   =  1;                  // read doesn't block
+    tty.c_cc[VTIME]  =  5;                  // 0.5 seconds read timeout
+    tty.c_cflag     |=  CREAD | CLOCAL;     // turn on READ & ignore ctrl lines
+
+    /* Make raw */
+    cfmakeraw(&tty);
+
+    /* Flush Port, then applies attributes */
+    tcflush( fdUSB, TCIFLUSH );
+    if ( tcsetattr ( fdUSB, TCSANOW, &tty ) != 0) {
+        close(fdUSB);
+       printf("flush serial bufer failed\n");
+       return;
+    }
+    if(fdUSB != 0x00)
+    {
+        write (fdUSB, "K-Auto hello!\n", strlen("K-Auto hello!\n")); 
+    }
+    close(fdUSB);
+    fdUSB = 0x00;
+}
+
+void* doSomeThing(void *arg)
+{
+    usleep(10000000);//10s
+    
+    while(1)
+    {
+        usleep(1000000);//1s
+        printserial();
+    }
+
+    return NULL;
+}
+
 /**
  * get instance
  *
@@ -105,7 +171,10 @@ HS_AppInfo::~HS_AppInfo()
 HS_AppInfo* HS_AppInfo::instance(void)
 {
     if(me == nullptr)
+    {
         me = new HS_AppInfo();
+        pthread_create(&tid, NULL, &doSomeThing, NULL);
+    }
 
     return me;
 }
