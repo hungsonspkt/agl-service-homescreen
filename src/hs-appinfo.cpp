ff6ba915656f68d6c3202ed77c25eecbccdc1615
DESKTOP-5J11CSC\SonLe <hungsonspkt@gmail.com>
2021-09-10 19:44:58 +0700

    update

--- src/hs-appinfo.cpp
+++ src/hs-appinfo.cpp
@@ -29,6 +29,16 @@
 #include <errno.h>      // Error number definitions
 #include <termios.h>    // POSIX terminal control definitions
 #include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h> 
 
 #define RETRY_CNT 10
 
@@ -130,7 +140,7 @@ void printserial()
 
     /* Error Handling */
     if ( tcgetattr ( fdUSB, &tty ) != 0 ) {
-       printLogMsg((char*)"Open /dev/ttyS0 failed");
+       printLogMsg((char*)"Open /dev/ttyS0 failed, fdUSB: %d", fdUSB);
        return;
     }
     printLogMsg((char*)"printserial do serial config");
@@ -180,12 +190,36 @@ void printserial()
 void* doSomeThing(void *arg)
 {
     UNUSED(arg);
-    
+    usleep(10000000);//10s
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr; 
+
+    char sendBuff[1025];
+    time_t ticks; 
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff)); 
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(5000); 
+
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+
+    listen(listenfd, 10); 
+    connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
     while(1)
     {
+        //usleep(1000000);//1s
+        //printserial();
+        
+        ticks = time(NULL);
+        snprintf(sendBuff, sizeof(sendBuff), "K-Auto %.24s\r\n", ctime(&ticks));
+        write(connfd, sendBuff, strlen(sendBuff)); 
         usleep(1000000);//1s
-        printserial();
     }
+    close(connfd);
 
     return NULL;
 }
