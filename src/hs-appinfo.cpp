ad697ef94cfeee86a31e9f5a9fdef0d41e5c18b1
DESKTOP-5J11CSC\SonLe <hungsonspkt@gmail.com>
2021-09-18 06:51:45 +0700

    update

--- src/hs-appinfo.cpp
+++ src/hs-appinfo.cpp
@@ -367,7 +367,7 @@ void* kAutoSerialComunication(void *arg)
         if(read(fdUSB, &inChar, 1) > 0x00)
         {
             memset(msgBuffer, 0x00, 0xFF);
-            sprintf((char*)msgBuffer, "received: %d, m_u8state: %d\n\r", inChar, m_u8state);
+            sprintf((char*)msgBuffer, "Data received: %d, m_u8state: %d\n\r", inChar, m_u8state);
             printLogMsg((char*)msgBuffer);
             switch(m_u8state)
             {
